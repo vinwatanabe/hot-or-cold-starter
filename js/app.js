3a25ab7cf88df0e9d1597e78836b6d2b8cec58f9
Vinicius Watanabe <vinicius_watanabe@hotmail.com>
2015-03-24 21:43:01 -0300

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -12,6 +11,34 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+    /* Pick a random number */
+    var rNumber = Math.round(Math.random()*100);
+    console.log(rNumber);
+
+    /* Defining variables */
+    
+    var count = 0;
+
+    function temperature(){
+      if (userGuess === rNumber){
+        $('#feedback').text("Yeah! You got it.");
+      }else{
+        $('#feedback').text("Cold")
+      }
+    }
+
+    /* Write the inserted value on the guess list */
+    $('#guessButton').click(function(){
+      var userGuess = $('#userGuess').val();
+      $('#guessList').append("<li>" + userGuess + "</li>");
+
+      temperature();
+
+      count++;
+      $('#count').text(count);
+
+      $('#userGuess').val("");
+
+      event.preventDefault();
+    });
 });
-
-
